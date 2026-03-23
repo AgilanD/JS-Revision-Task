024aebc392bbd148495b5f547b9f2d00a438133e
AgilanD <agilan.d@mitrahsoft.in>
2026-03-23 11:26:07 +0530

    Ubdated

--- 26.Filter_List_Search/Task26.js
+++ 26.Filter_List_Search/Task26.js
@@ -1,19 +1,21 @@
 const btn=document.getElementById("input");
 const btns=document.getElementById("inputs");
-const arr=["Man",1,"Hello","WElcome",512]
+const arr=["Man","1","Hello","WElcome","512"]
 
 btns.addEventListener('click',()=>{
 
     let data=arr.filter((val,cur,arr)=>{
-        if(arr[cur]==btn.value){
+        if(arr[cur]===btn.value){
              return val
         }
     })
     console.log(data);
-    if(data){
+    if(data.length!==0){
         document.getElementById("Result").innerHTML="SuccessFull"
     }
-
+    else{
+        document.getElementById("Result").innerHTML="Item Not Present"
+    }
 
 })
 
